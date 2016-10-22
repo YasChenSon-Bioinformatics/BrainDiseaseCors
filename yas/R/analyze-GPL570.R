940b7c37878bf038dd7883d07bfec30d7740ae34
caprice-j <skyfly0008@gmail.com>
2016-10-21 20:06:09 -0400

    finish 1st binary analysis (details below)
    I selected out three datasets, GDS4523, GDS4231, and GDS1917 due to preprocessing easiness.
    (Other datasets do not have the 'disease.state' column, or lacked a lot of gene expression values, and so on)
    
    GDS4523 and GDS1917 deal with schizophrenia while GDS4231 deal with HIV.
    
    the final result is in a variable 'related_genes_df'.
    My hypothesis is that the two schizophrenia datasets would make same genes significant and corroborate our procedure correctness
    (Hopefully the HIV genes too). Unfortunately, this time both did not find any related genes.
    Only HIV datasets reoprts some significant (adjusted p < .05) genes.

--- yas/R/analyze-GPL570.R
+++ yas/R/analyze-GPL570.R
@@ -22,20 +22,44 @@ for ( no in all_GDSv ) {
     }
     #Sys.sleep(5)
     if( ! thisGDS@header$platform %in% c('GPL570') ) {
-        message('Skip GDS ', no, ' because of platform')
+        message('     Skip GDS ', no, ' because of platform')
         next
     }
     if( ! 'disease.state' %in% colnames(thisGDS@dataTable@columns) ){
-        message('Skip GDS ', no, ' because of no disease.state')
+        message('     Skip GDS ', no, ' because of no disease.state')
+        next
+    }
+    thisGDS@dataTable@columns$disease.state <- gsub(' ','_',thisGDS@dataTable@columns$disease.state)
+    if( ! any(grepl('control', thisGDS@dataTable@columns$disease.state)) ){
+        message('     Skip GDS ', no, ' because of no control')
+        next
+    }
+    if( length(unique(thisGDS@dataTable@columns$disease.state)) != 2 ){
+        message('     Skip GDS ', no, ' because of control values')
+        next
+    }
+    if( any(is.na(thisGDS@dataTable@table)) ) {
+        message('     Skip GDS ', no, ' because of NA')
         next
     }
     all_GDS[[i]] <- thisGDS
     i <- i + 1
 }
 
+function(){
+    tmpdf <- sapply(all_GDS, function(x){x@dataTable@table[, 4] %>% as.numeric} )  %>% as.data.frame
+    colnames(tmpdf) <- sapply(all_GDS, function(x){x@header$dataset_id[1]} )
+    tmpdf <- tmpdf %>% gather(gds,eval,starts_with('GDS'))
+    pdf("/Users/PCUser/Dropbox/CU2016/F16CLASSES/TB_Tatonetti/BrainDiseaseCors/yas/EDA/expValues-logOrNonLog1stEd.pdf")
+    for( g in unique(tmpdf$gds) ) {
+        p <- ggplot(tmpdf %>% filter(gds == g )) + geom_density(aes(x=eval), fill="red", alpha=.5) + labs(title=g)
+        print(p)
+    }
+    dev.off()
+}
+
 # Note: some rows like 1320_at or 1405_i_at in GDS4522 are <NA>.
 # 
-
 GDS_on_GPL570v <- sapply( all_GDS, function(gds) {gds@header$dataset_id[1]} )
 
 # all GDS in all_GDSv               ~ 2.5 GB
@@ -44,10 +68,27 @@ GDS_on_GPL570v <- sapply( all_GDS, function(gds) {gds@header$dataset_id[1]} )
 # Extract all to ESETs
 all_ESET <- list()
 for (i in seq_along(all_GDS)) {
-    all_ESET[[i]] <- GDS2eSet(GDS=all_GDS[[i]], do.log2 = FALSE)
+    all_ESET[[i]] <- GDS2eSet(GDS=all_GDS[[i]], do.log2 = TRUE)
     
-    # Note: log2(x) is undefined and return NaN if x is negative.
+    if( any(is.nan(exprs(all_ESET[[i]]))) ){
+        # Note: log2(x) is undefined and return NaN if x is negative.
+        # What's worse, some datasets looks like already applied log2 transformaiton.
+        message("    ", all_GDS[[i]]@header$dataset_id[1], " seems already applied log2(). SKIP")
+        all_ESET[[i]] <- GDS2eSet(GDS=all_GDS[[i]], do.log2 = FALSE)
+    }
 }
+
+function(){
+    expl <- sapply(all_ESET, function(x){ exprs(x) } )
+    pdf("/Users/PCUser/Dropbox/CU2016/F16CLASSES/TB_Tatonetti/BrainDiseaseCors/yas/EDA/EsetVal1stEd.pdf")
+    l <- expl[[1]]
+    for( l in expl ) {
+        print(hist(l))
+    }
+    dev.off()
+}
+
+
 # all_ESET in all_GDSv              ~ 1.6 GB
 # only GDS with the platform GPL570 ~ 0.7 GB 
 
@@ -78,6 +119,7 @@ for (i in seq_along(all_ESET)) {
     
     all_M[[i]] <- as.matrix( mapped %>% dplyr::select(-uni) )  # e.g. [ 65107 genes x 41 SaMples ]
     rownames(all_M[[i]]) <- mapped$uni
+    all_M[[i]] <- all_M[[i]] %>% na.omit
     attr(all_M[[i]], newAttrName) <- all_ESET[[i]]@experimentData@other$dataset_id[1]
     
     #all_DF[[i]] <- as.data.frame(all_ESET[[i]]) 
@@ -100,56 +142,8 @@ for (i in seq_along(all_ESET)) {
 # all_M with the platform GPL570  ~ 300 MB
 
 sapply(all_M, dim)
-sapply(all_M, function(x) {sum(is.na(x))} )
-
-######################################################################
-# Notes:
-
-# Certain GDS's have        Funny column names
-# 1085, 1813, 3834      "x1", "x2", etc --> what probes are these?
-# 1835                  x882, x880
-# 3113                  x156427, x139282
-# 4758, 4879            X7896736
-# 833                   175019765, 175019766
-# 909, 910              x559, x558 
-
-
-# Limit our analyses to GPL570 datasets (most common, and most comprehensive array)
-#gpl570 = list(df_5204,df_4838,df_4532,df_4523,df_4522,df_4477,df_4358,df_4231,df_4218,df_4136,df_4135,df_3502,df_2821,df_2795,df_2154,df_1962,df_1917)
-# Done in above
-
-
-# Find NAs, NANs (NANs are a todo)
-nan_count <-function (x) sapply(x, function(y) sum(is.nan(y)))
-i <- 1 
-for (i in seq_along(all_M) ) {
-    x <- all_M[[i]]
-    message(sprintf("%s %6d x %3d [ %5s ] ", attr(x, newAttrName), nrow(x), ncol(x), any(is.na(x))))
-    # print(any(is.nan(df)))
-}
-# why some entries are lacked? like
-
-#####################################################################
-# Notes:
-
-# gpl570 dataset        variable count        NAs?        NANs?
-# df_5204                   54679               n           
-# df_4838                   54678               n
-# df_4532                   54680               n
-# df_4523                   45680               n
-# df_4522                   54680               y
-# df_4477                   54678               y
-# df_4358                   54679               n
-# df_4231                   54679               y
-# df_4218                   54680               y
-# df_4136                   54679               n
-# df_4135                   54679               y
-# df_3502                   54681               y
-# df_2821                   54679               y
-# df_2795                   54679               n
-# df_2154                   54678               n
-# df_1962                   54679               y
-# df_1917                   54678               n
+sapply(all_M, function(x) {sum(is.na(x))} )  # MAYBE-LATER What's these nas?
+sapply(all_GDS, function(x) {message(paste0(unique(x@dataTable@columns$disease.state),collapse="\t"))} )  # MAYBE-LATER What's these nas?
 
 # Find the probes common to all df's
 
@@ -172,25 +166,27 @@ for (i in seq_along(all_M) ) {
 
 library(dplyr)
 library(tidyr)
-# experimental condition vector
-exp_condv <- sapply( all_GDS, function(gds) { colnames(gds@dataTable@columns) }) %>% unlist %>% unique 
-cond_df <- matrix(data = 0, # all-zero amtrix
-                  nrow = length(all_GDS),
-                  ncol = length(exp_condv),
-                  dimnames = list(GDS_on_GPL570v, exp_condv)) %>% data.frame
-for ( i in seq_along(all_GDS) ){
-    exp_cond_of_this_dataset <- colnames(all_GDS[[i]]@dataTable@columns)
-    cond_df[i, ] <- as.numeric(exp_condv %in% exp_cond_of_this_dataset)
+function(){ # Pre-Analysis: What experimental condition each GDS has?
+    # experimental condition vector
+    exp_condv <- sapply( all_GDS, function(gds) { colnames(gds@dataTable@columns) }) %>% unlist %>% unique 
+    cond_df <- matrix(data = 0, # all-zero amtrix
+                      nrow = length(all_GDS),
+                      ncol = length(exp_condv),
+                      dimnames = list(GDS_on_GPL570v, exp_condv)) %>% data.frame
+    for ( i in seq_along(all_GDS) ){
+        exp_cond_of_this_dataset <- colnames(all_GDS[[i]]@dataTable@columns)
+        cond_df[i, ] <- as.numeric(exp_condv %in% exp_cond_of_this_dataset)
+    }
+    cdf <- cond_df %>% add_rownames(var='GDS') %>% gather(key = meta, value = bit, -starts_with('GDS'))
+    experimental_bitmap <-
+        left_join(cond_df %>% add_rownames(var='GDS') %>% gather(key = meta, value = bit, -matches('GDS')),
+                  cdf %>% group_by(meta) %>% summarize( nentry = sum(bit) ), by="meta") %>%
+        mutate(meta=paste(nentry,meta)) %>% ggplot(aes(x=gsub('GDS','',GDS), y=reorder(meta,nentry))) +
+        geom_tile(aes(fill=as.factor(bit))) + scale_fill_grey()
+    print(experimental_bitmap)
 }
-cdf <- cond_df %>% add_rownames(var='GDS') %>% gather(key = meta, value = bit, -starts_with('GDS'))
-experimental_bitmap <-
-    left_join(cond_df %>% add_rownames(var='GDS') %>% gather(key = meta, value = bit, -matches('GDS')),
-              cdf %>% group_by(meta) %>% summarize( nentry = sum(bit) ), by="meta") %>%
-    mutate(meta=paste(nentry,meta)) %>% ggplot(aes(x=gsub('GDS','',GDS), y=reorder(meta,nentry))) +
-    geom_tile(aes(fill=as.factor(bit))) + scale_fill_grey()
-print(experimental_bitmap)
 
-selectConditionv <- function( m, g = 'GDS5204' ) {
+selectConditionv <- function( m, g = 'GDS5204' ) { # MAYBE-LATER not implemented yet
     condition <- list()
     if ( g %in% paste0('GDS', c(1917, 1962, 2154, 2795, 2821, 3502, 4135, 4136, 4218, 4231,
                                 4358, 4522, 4523, 4838)) ) {
@@ -206,11 +202,10 @@ selectConditionv <- function( m, g = 'GDS5204' ) {
     return(condition)
 }
 
-# Compute correlations within samples from each dataset
-disease_ms = list()
-i <- k <- 2
+out = list()
+i <- k <- 3
 for (k in seq_along(all_M)) {
-    
+
     m <- all_M[[k]]
     g <- all_GDS[[k]]
     # Subset to just DZ instances
@@ -223,82 +218,27 @@ for (k in seq_along(all_M)) {
     }else {
         message()
     }
+
+    dMatrix <- model.matrix( ~ g@dataTable@columns$disease.state )
+    
+    lmfitted <- limma::lmFit(m, design = dMatrix) # plot(lmfitted$coefficients, pch=18) what's this?
+    ebayesed <- eBayes(lmfitted)
+    tabled <- topTable(ebayesed, number = 1000)#, p.value = .05)
+    tabled
+    out[[k]] <- list( lm = lmfitted, table = tabled )
 }
 
-disease_samples <-
-    g@dataTable@columns[ g@dataTable@columns[, condition$col ] != condition$val, ]$sample
-
-# NB: some GDSs do not have disease.state like GDS5204 (aging)
-disease_ms[[i]] = m[ , colnames(m) %in% disease_samples]
-i <- i + 1
+tmp_rld <- out[[1]]$table %>% add_rownames('gene') %>% select(gene)
+for (k in seq_along(all_M)) {
+    options(scipen = 10000)
+    m <- all_M[[k]]
+    
+    this <-
+        out[[k]]$table %>% select(P.Value, adj.P.Val) %>%
+        add_rownames('gene') %>% mutate_each(funs( r = round(.,5)*100), P.Value, adj.P.Val) %>%
+        unite_(col = paste0(attr(m, 'GDS'),'_percent'), c('P.Value', 'adj.P.Val'), sep=" -> ")
+    
+    tmp_rld <- full_join(tmp_rld, this, by="gene")
 }
-
-
-
-# Compute correlations within samples from each dataset
-cors = list()
-for (i in seq_along(disease_ms) ) {
-    message(i, "-----------------------------------------------")
-    # Compute corr
-    corre = cor(disease_ms[[i]])
-    cors[[i]] = corre
-}
-
-names(cors) = c('df_5204','df_4838','df_4532','df_4523','df_4522','df_4477','df_4358','df_4231','df_4218','df_4136','df_4135','df_3502','df_2821','df_2795','df_2154','df_1962','df_1917')
-attach(cors)
-par(mfrow=c(3,3))
-par(mar=c(1,1,1,1))
-plot(cors[[2]], main="asdf")
-plot(cors[[4]], main="asdf")
-plot(cors[[7]], main="asdf")
-plot(cors[[10]], main="asdf")
-plot(cors[[14]], main="asdf")
-plot(cors[[15]], main="asdf")
-plot(cors[[17]], main="asdf")
-
-
-
-# Average across rows for each disease state
-avgs = list()
-i = 1
-for (df in disease_dfs) {
-    print(i)
-    print(dim(df))
-    print("-----------------------------------------------")
-    colMeans = colMeans(df, na.rm=TRUE)
-    avgs[[i]] = colMeans
-    i = i + 1
-}
-
-
-
-require(reshape2)
-worked = list(4,5,7,8,9,10,11,12,13,14,15,16,17) # (also 2 worked)
-worked_2 = list(9,11,14,15,16)
-avg_dz_exp_vals = as.data.frame(avgs[2])
-for (i in worked_2) {
-    avg_dz_exp_vals <- cbind(avg_dz_exp_vals, as.data.frame(avgs[i]))
-}
-
-
-
-install.packages("corrplot")
-library(corrplot)
-# Compute correlation between diseases expression values
-c = cor(avg_dz_exp_vals, use="complete.obs")
-names = c("GDS4838 - Neuroectodermal Tumors",
-          "GDS4218 - Multiple Sclerosis Brain Lesions",
-          "GDS4135 - Temporal Cortex, Ageing Study",
-          "GDS2795 - Alzheimer's Disease",
-          "GDS2154 - Inflammatory Dilated Cardiomyopathy",
-          "GDS1962 - Glioma tissue")
-colnames(c) <- rownames(c) <- names
-corrplot.mixed(c, t1.pos="r", t1.col="blue", c1.srt=60, c1.pos="r", cl.align.text="r", mar=c(1,1,1,1), height=1600, width=1600)
-
-
-
-
-pData(ESET_3502)
-exprs(ESET_3502)
-assayDataElement(ESET_3502)
-
+related_genes_df <- tmp_rld %>% unite(str, starts_with('GDS'), remove = FALSE) %>% filter( nchar(str) > 25 ) %>% select(-str)
+#table(sapply(out, function(x){rownames(x$table)}))
