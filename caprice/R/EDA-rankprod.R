8b2bb2eb8f9b01f81054ee1e8c7a5f86ee7f0e4f
caprice-j <skyfly0008@gmail.com>
2016-10-29 06:30:08 +0000

    add Report-1-limma.Rmd

--- caprice/R/EDA-rankprod.R
+++ caprice/R/EDA-rankprod.R
@@ -38,22 +38,29 @@ ESETl <- convertGDS2ESET(allGDSl)
 
 Ml <- extractMatrixFromEset(ESETl)
 
-# CAUTION: create ~ 3gb output file.
+library(RankProd)
+
+# CAUTION: 100 MB, but without huge=TRUE output file is ~ 3gb.
+startTime <- proc.time()
 res <- RankProducts(data=exprs(ESETl[[3]]),
                     cl = allGDSl[[3]]@dataTable@columns$disease.state %>% as.factor() %>% as.numeric - 1,
                     gene.names = rownames(exprs(ESETl[[3]])),
                     logged = TRUE,
                     na.rm=FALSE,
                     plot=FALSE,
-                    rand=123
+                    rand=123, 
+                    huge=TRUE
                     )
+endTime <- proc.time()
+endTime - startTime
+
 
 par(pch=20)
 par(cex=.1)
 par(col="black")
 plotRP(res, cutoff=.05)
 
-toped <- topGene(res, gene.names = rownames(exprs(ESETl[[3]])), num.gene = 100)
+toped <- topGene(res, logged=TRUE, gene.names = rownames(exprs(ESETl[[3]])), cutoff = .05)
 
 
 plot(exprs(ESETl[[3]])[rownames(exprs(ESETl[[3]])) == '236995_x_at'], col=factor(allGDSl[[3]]@dataTable@columns$disease.state))
@@ -68,7 +75,9 @@ plot(exprs(ESETl[[3]])[rownames(exprs(ESETl[[3]])) == '1557993_at'], col=factor(
 
 allGDSl[[3]]@dataTable@table %>% filter( ID_REF == '1557993_at') %>% select(-ID_REF, -IDENTIFIER) %>% c %>% unlist %>% as.numeric %>% log2 %>% plot(., col=factor(allGDSl[[3]]@dataTable@columns$disease.state))
 
-
+tmp <- as.data.frame(exprs(ESETl[[3]])) %>% rownames_to_column('probe') %>% filter( probe %in% rownames(toped$Table1)[1:30] ) %>% gather(sample, eval, starts_with('GSM')) %>% left_join(., allGDSl[[3]]@dataTable@columns %>% select(sample, disease.state) %>% mutate(sample = as.character(sample) ), by="sample") 
+p <- ggplot(tmp) + geom_point(aes(x=sample,y=eval,color=disease.state)) + facet_wrap( ~ probe )
+ggsave(filename = paste0(gcl$rootDir, '/caprice/EDA/RankProd-GDS1917-Table1-30.png'), plot=p, width = 16, height=10)
 
 # CAUTION: create ~ 3gb output file.
 generes <- RankProducts(data=Ml[[3]],
@@ -77,8 +86,7 @@ generes <- RankProducts(data=Ml[[3]],
                     logged = TRUE,
                     na.rm=FALSE,
                     plot=FALSE,
-                    rand=123
-)
+                    rand=123)
 
 plotRP(generes, cutoff=.05)
 
