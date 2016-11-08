c13cc3a2f0735e0fd94f0bc965311591b8332bee
caprice-j <skyfly0008@gmail.com>
2016-11-08 13:19:01 -0500

    modify DIAG-241672.R for printing

--- caprice/R/DIAG-241672_at.R
+++ caprice/R/DIAG-241672_at.R
@@ -14,5 +14,9 @@ intersect(rownames(out[[1]]$table), rownames(out[[2]]$table))
 p <- '241672_at'
 
 par(mfrow=c(1,2))
-plot(Ml[[1]][p, ], col=as.factor(GDSl[[1]]@dataTable@columns$disease.state=='non-tumor'), main=paste0(p, ' in GDS1962 (Tumor Study)\n Red: non-tumor'))
-plot(Ml[[2]][p, ], col=as.factor(GDSl[[2]]@dataTable@columns$age%in%c('young (<40yr)', 'middle aged (40-70yr)')), main=paste0(p, ' in GDS5204 (Aging study)\n Red: non-elderly(<70yr)'))
+plot(Ml[[1]][p, ], lwd=3, col=as.factor(GDSl[[1]]@dataTable@columns$disease.state=='non-tumor'),
+     pch=as.numeric(GDSl[[1]]@dataTable@columns$disease.state=='non-tumor')+2,
+     main=paste0(p, ' in GDS1962 (Tumor Study)\n Red-Plus: non-tumor'), ylab="Expression Values")
+plot(Ml[[2]][p, ], col=as.factor(GDSl[[2]]@dataTable@columns$age%in%c('young (<40yr)', 'middle aged (40-70yr)')), 
+     ch=as.numeric(GDSl[[2]]@dataTable@columns$age%in%c('young (<40yr)', 'middle aged (40-70yr)'))+2,
+     main=paste0(p, ' in GDS5204 (Aging study)\n Red-Plus: non-elderly(<70yr)'), ylab="Expression Values", lwd=3)
